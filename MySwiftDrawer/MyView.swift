ef4534895fd9a8186e81e95b4e7ef20d13ba2bca
Brad <brad@brad.tw>
2017-09-11 14:15:47 +0800

    2017091101

--- MySwiftDrawer/MyView.swift
+++ MySwiftDrawer/MyView.swift
@@ -9,18 +9,43 @@
 import Cocoa
 
 class MyView: NSView {
-
+    
+    var line:[CGPoint] = [] // Array<CGPoint>
+    
+    // MyView 的呈現畫面
     override func draw(_ dirtyRect: NSRect) {
+        // 一條線
+        if line.count<2 {return}
         
-        var myPath = NSBezierPath()
-        myPath.move(to: CGPoint(x: 0, y: 0))
-        myPath.line(to: CGPoint(x: 100, y: 100))
-        myPath.line(to: CGPoint(x: 150, y: 50))
+        let myPath = NSBezierPath()
+        myPath.move(to: line[0])
+        for i in 1..<line.count {
+            myPath.line(to: line[i])
+        }
         myPath.stroke()
         
-        
-        
+    }
+    override func mouseDown(with event: NSEvent) {
+        let px = event.locationInWindow.x
+        let py = event.locationInWindow.y
+        line += [CGPoint(x: px,y: py)]
+    }
+    override func mouseUp(with event: NSEvent) {
+        let px = event.locationInWindow.x
+        let py = event.locationInWindow.y
+        line += [CGPoint(x: px,y: py)]
         
     }
+    override func mouseDragged(with event: NSEvent) {
+        let px = event.locationInWindow.x
+        let py = event.locationInWindow.y
+        line += [CGPoint(x: px,y: py)]
+        needsDisplay = true
+    }
+    
+    
+    
+    
+    
     
 }
